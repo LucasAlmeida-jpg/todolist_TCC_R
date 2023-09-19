b7f552c9ab17e38567b5b0e5e54d5896db062191
LucasAlmeida-jpg <lucasalmeidagouvea123@gmail.com>
2023-09-19 16:41:23 -0300

    first commit

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -4,6 +4,7 @@ import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
 import { EditTodoForm } from "./EditTodoForm";
 
+
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
 
@@ -42,9 +43,9 @@ export const TodoWrapper = () => {
 
   return (
     <div className="TodoWrapper">
-      <h1>Get Things Done !</h1>
+      <h1>TO DO LIST</h1>
       <TodoForm addTodo={addTodo} />
-      {/* display todos */}
+      {}
       {todos.map((todo) =>
         todo.isEditing ? (
           <EditTodoForm editTodo={editTask} task={todo} />
